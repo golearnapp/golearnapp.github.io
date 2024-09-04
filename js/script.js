bcbe3957a7f13789a7cd555fb2a20f5a8ad058e7
gabrielbotandev <gabrielficciobotan@gmail.com>
2024-09-03 22:37:18 -0300

    fix: adjust phone input placeholder and sign-up button value, add phone number formatting
    - Fixed the phone input placeholder and the value of the 'Sign Up' button.
    - Applied JavaScript code to format the phone number in the phone input field.

--- js/script.js
+++ js/script.js
@@ -47,3 +47,16 @@ function hideCounter() {
 function scrollToTop() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
+
+/*--------------------------------------*/
+/*          Telephone Format            */
+/*--------------------------------------*/
+
+document.getElementById("telefone").addEventListener("input", function (e) {
+  var x = e.target.value
+    .replace(/\D/g, "")
+    .match(/(\d{0,2})(\d{0,5})(\d{0,4})/);
+  e.target.value = !x[2]
+    ? x[1]
+    : "(" + x[1] + ") " + x[2] + (x[3] ? "-" + x[3] : "");
+});
